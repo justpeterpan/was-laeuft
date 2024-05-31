de2d8bc79122d31b28b2ae54a24db8969292a8d4
justpeterpan <peter.busch@posteo.net>
2024-06-01 01:01:50 +0200

    fix: get date correctly from params

--- middleware/redirect.ts
+++ middleware/redirect.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const date = new Date()
   const today = `${date.getFullYear()}${(date.getMonth() + 1)
     .toString()
-    .padStart(2, '0')}${date.getDate()}`
+    .padStart(2, '0')}${date.getDate().toString().padStart(2, '0')}`
   if (to.path !== '/' && to.path.slice(1) < init) return navigateTo('/')
   if (to.path !== '/' && to.path.slice(1) > today) return navigateTo('/')
 })
